b9a2e3a1d3b55ca135ebe25523d6b66c83c05a7e
ivsemionov <ivsemionov@iggn.permkrai.ru>
2021-06-09 18:04:28 +0500

    возможность обновления пользователя

--- chat/services.py
+++ chat/services.py
@@ -1,7 +1,7 @@
 from typing import Union
 
 import sqlalchemy
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy.exc import IntegrityError, NoResultFound
 
 from chat.models import User, Chat, Message
 from database import async_db_session
@@ -10,6 +10,9 @@ from helpers.exceptions import HandledException
 
 class UserAlreadyExistsException(HandledException):
     text = 'Пользователь с таким логином уже зарегистрирован'
+
+class UserDoesNotExistsException(HandledException):
+    text = 'Пользователь с такими данными не найден'
 
 class UserService:
     @classmethod
@@ -22,7 +25,12 @@ class UserService:
 
     @classmethod
     async def update(cls, item_id, **kwargs):
-        pass
+        try:
+            await User.update(item_id,**kwargs)
+            return await User.get(item_id)
+        except NoResultFound:
+            raise UserDoesNotExistsException
+
 
 
 class ChatService:
--- chat/tests/test_services.py
+++ chat/tests/test_services.py
@@ -8,7 +8,8 @@ from unittest import IsolatedAsyncioTestCase
 from app import setup
 from chat.models import User
 
-from chat.services import UserService, ChatService, MessageService, UserAlreadyExistsException
+from chat.services import UserService, ChatService, MessageService, UserAlreadyExistsException, \
+    UserDoesNotExistsException
 from database import async_db_session
 
 
@@ -26,7 +27,6 @@ class BaseTest(IsolatedAsyncioTestCase):
 
 
 class UserServiceTest(BaseTest):
-
     async def test_creating_user(self):
         user = await UserService.create(login='user', password='password')
         created_user = await User.get(user.id)
@@ -34,10 +34,27 @@ class UserServiceTest(BaseTest):
 
     async def test_raise_error_if_user_already_exists(self):
         user = await UserService.create(login='user', password='password')
-        created_user = await User.get(user.id)
         try:
             user = await UserService.create(login='user', password='password')
         except UserAlreadyExistsException:
             pass
         else:
             raise AssertionError
+
+    async def test_updating_user(self):
+        user = await UserService.create(login='user', password='password')
+        user = await UserService.update(user.id ,login='new_user', password='new_password')
+        updated_user = await User.get(user.id)
+        self.assertEqual(updated_user.login, 'new_user')
+        self.assertEqual(updated_user.password, 'new_password')
+
+    async def test_updating_not_existing_user_raise_error(self):
+        not_existing_id = 999
+        try:
+            user = await UserService.update(not_existing_id ,login='new_user', password='new_password')
+        except UserDoesNotExistsException:
+            pass
+        else:
+            raise AssertionError
+
+
